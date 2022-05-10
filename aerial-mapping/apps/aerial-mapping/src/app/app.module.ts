fea87945ce4a8acaaa5feb685dbd6b1ed9c5cc77
dylan2894 <61905387+dylan2894@users.noreply.github.com>
2022-05-10 19:07:09 +0200

    ✨ (ui): Adding app routing

--- aerial-mapping/apps/aerial-mapping/src/app/app.module.ts
+++ aerial-mapping/apps/aerial-mapping/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule
   ],
   providers: [],
--- aerial-mapping/libs/client/dashboard/src/lib/client-dashboard.module.ts
+++ aerial-mapping/libs/client/dashboard/src/lib/client-dashboard.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardViewComponent } from './dashboard-view/dashboard-view.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule
+  ],
   declarations: [DashboardViewComponent],
 })
 export class ClientDashboardModule {}
