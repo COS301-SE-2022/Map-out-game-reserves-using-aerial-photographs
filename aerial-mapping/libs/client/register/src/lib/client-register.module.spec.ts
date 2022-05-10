9fb6d666f43137c1e63899d6fb0f3fb455207eb1
dylan2894 <61905387+dylan2894@users.noreply.github.com>
2022-05-10 19:06:18 +0200

    ✨ (ui): Adding Register UI

---
+++ aerial-mapping/libs/client/register/src/lib/client-register.module.spec.ts
@@ -0,0 +1,18 @@
+import { async, TestBed } from '@angular/core/testing';
+import { ClientRegisterModule } from './client-register.module';
+
+describe('ClientRegisterModule', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ClientRegisterModule],
+    }).compileComponents();
+  }));
+
+  // TODO: Add real tests here.
+  //
+  // NB: This particular test does not do anything useful.
+  //     It does NOT check for correct instantiation of the module.
+  it('should have a module definition', () => {
+    expect(ClientRegisterModule).toBeDefined();
+  });
+});
