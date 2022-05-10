be2a2b4e0ca4f8a121ff9ac941934818dabc06c2
dylan2894 <61905387+dylan2894@users.noreply.github.com>
2022-05-10 19:05:40 +0200

    ✨(ui): Adding Login UI

---
+++ aerial-mapping/libs/client/login/src/lib/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'aerial-mapping-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  constructor() {
+    //Code
+  }
+
+  ngOnInit(): void {}
+}
