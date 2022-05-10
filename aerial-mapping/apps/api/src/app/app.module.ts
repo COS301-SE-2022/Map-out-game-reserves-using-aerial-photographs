a68de96767fb4f06580bfa37e82dfd509299878b
dylan2894 <61905387+dylan2894@users.noreply.github.com>
2022-05-10 15:20:28 +0200

    🧱(api): Adding GraphQL, Apollo Server and Prisma

--- aerial-mapping/apps/api/src/app/app.module.ts
+++ aerial-mapping/apps/api/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
