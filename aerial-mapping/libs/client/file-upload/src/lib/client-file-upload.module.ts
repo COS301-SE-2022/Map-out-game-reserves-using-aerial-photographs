25203af62145a1601ac6d126124315e4fd82bf79
OomBen <u17030553@tuks.co.za>
2022-05-11 15:17:51 +0200

    Added upload component. Upload by button works, but not upload by drag and drop

--- aerial-mapping/libs/client/file-upload/src/lib/client-file-upload.module.ts
+++ aerial-mapping/libs/client/file-upload/src/lib/client-file-upload.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { FileUploadRoutingModule } from './file-upload-routing.module';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { ComponentsNavbarModule } from './../../../shared/components/navbar/src/lib/client-shared-components-navbar.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FileUploadRoutingModule,
     MatCardModule,
+    MatInputModule,
+    MatButtonModule,
     ComponentsNavbarModule
   ],
   declarations: [FileUploadComponent],
