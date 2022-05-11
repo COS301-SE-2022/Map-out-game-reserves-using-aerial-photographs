b6be68b1f746335a2bd6a4d2252c3203170a7f4b
dylan2894 <61905387+dylan2894@users.noreply.github.com>
2022-05-11 23:15:34 +0200

    🚚(ui): Fixing route for Account and MapCollection

--- aerial-mapping/libs/client/mapcollection/src/lib/client-mapcollection.module.ts
+++ aerial-mapping/libs/client/mapcollection/src/lib/client-mapcollection.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapcollectionComponent } from './mapcollection/mapcollection.component';
+import { MapcollectionRoutingModule } from './mapcollection-routing.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MapcollectionRoutingModule
+  ],
   declarations: [MapcollectionComponent],
   exports: [MapcollectionComponent]
 })
