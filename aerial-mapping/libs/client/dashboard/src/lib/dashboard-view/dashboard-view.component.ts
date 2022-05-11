d0d38156a1f06cc2593369b6e5a18b7638c90732
Dylan Spies <u20432748@tuks.co.za>
2022-05-11 22:45:37 +0200

    Change to photos and Messages

--- aerial-mapping/libs/client/dashboard/src/lib/dashboard-view/dashboard-view.component.ts
+++ aerial-mapping/libs/client/dashboard/src/lib/dashboard-view/dashboard-view.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faPaperPlane as plane } from '@fortawesome/free-solid-svg-icons';
+import { faCamera as camera } from '@fortawesome/free-solid-svg-icons';
 import { BarChart } from '../bar-chart/bar-chart.model';
 
 @Component({
@@ -8,46 +8,43 @@ import { BarChart } from '../bar-chart/bar-chart.model';
   styleUrls: ['./dashboard-view.component.scss'],
 })
 export class DashboardViewComponent {
-  //Values from API
-  pastWeek = [3, 5, 2, 3, 2, 1, 7]; //From a week ago - today
-  total = 253;
+  pastWeek: number[];
+  total: number;
+  public messages: string[];
+  public photos: Array<BarChart>;
 
-  plane = plane;
+  camera = camera;
 
   count = 0;
+  d = new Date().getDay();
 
-  public flights: Array<BarChart> = [
-    { Value: this.pastWeek[this.count++], Size: '', Legend: getDayOfWeek() },
-    { Value: this.pastWeek[this.count++], Size: '', Legend: getDayOfWeek() },
-    { Value: this.pastWeek[this.count++], Size: '', Legend: getDayOfWeek() },
-    { Value: this.pastWeek[this.count++], Size: '', Legend: getDayOfWeek() },
-    { Value: this.pastWeek[this.count++], Size: '', Legend: getDayOfWeek() },
-    { Value: this.pastWeek[this.count++], Size: '', Legend: getDayOfWeek() },
-    { Value: this.pastWeek[this.count++], Size: '', Legend: getDayOfWeek() },
-  ];
-}
-
-let d = new Date().getDay();
-
-function getDayOfWeek(): string{
-  if (d == new Date().getDay() - 1){
-    d++;
-    return "Today";
+  constructor() {
+    this.pastWeek = [3, 5, 2, 3, 2, 1, 7];
+    this.total = 253;
+    this.messages = ["Everything is good", "System back online", "System has halted!!!"];
+    this.photos = [
+      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
+      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
+      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
+      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
+      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
+      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
+      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
+    ];
   }
-  const weekday = [
-    'Mon',
-    'Tue',
-    'Wed',
-    'Thu',
-    'Fri',
-    'Sat',
-    'Sun',
-  ];
 
-  if (d == 6){
-    d = d - 6;
-    return weekday[6];
-  } else {
-    return weekday[d++];
+  getDayOfWeek(): string {
+    if (this.d == new Date().getDay() - 1) {
+      this.d++;
+      return 'Today';
+    }
+    const weekday = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+    if (this.d == 6) {
+      this.d = this.d - 6;
+      return weekday[6];
+    } else {
+      return weekday[this.d++];
+    }
   }
 }
