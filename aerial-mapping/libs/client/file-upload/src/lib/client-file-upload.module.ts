4eadc6e6e1d856b8a86d4a0a72b757b2400a675e
OomBen <u17030553@tuks.co.za>
2022-05-11 11:10:55 +0200

    Added empty File Upload page and navbar component

--- aerial-mapping/libs/client/file-upload/src/lib/client-file-upload.module.ts
+++ aerial-mapping/libs/client/file-upload/src/lib/client-file-upload.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { FileUploadRoutingModule } from './file-upload-routing.module';
+import {MatCardModule} from '@angular/material/card';
+import { ComponentsNavbarModule } from './../../../shared/components/navbar/src/lib/client-shared-components-navbar.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    FileUploadRoutingModule
+    FileUploadRoutingModule,
+    MatCardModule,
+    ComponentsNavbarModule
   ],
   declarations: [FileUploadComponent],
 })
