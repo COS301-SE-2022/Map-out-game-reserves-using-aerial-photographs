324671877095bf40c24e5235cba0eeaec70e6d34
OomBen <u17030553@tuks.co.za>
2022-05-12 10:47:36 +0200

    Added routing. Landing page now Login page

--- aerial-mapping/apps/aerial-mapping/src/app/app.component.ts
+++ aerial-mapping/apps/aerial-mapping/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'aerial-mapping-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor() { }
+export class AppComponent implements OnInit {
+  constructor(private router : Router) { }
+
+  ngOnInit(): void {
+      this.router.navigate(['login'])
+  }
 }
