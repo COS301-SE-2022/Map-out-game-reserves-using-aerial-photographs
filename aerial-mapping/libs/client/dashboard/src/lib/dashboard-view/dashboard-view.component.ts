231a1f11ad534435a2ca5ce2d1dae0be5c5aa679
Dylan Spies <u20432748@tuks.co.za>
2022-05-12 15:49:12 +0200

    Icons on messages and Complete

--- aerial-mapping/libs/client/dashboard/src/lib/dashboard-view/dashboard-view.component.ts
+++ aerial-mapping/libs/client/dashboard/src/lib/dashboard-view/dashboard-view.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { faCamera as camera } from '@fortawesome/free-solid-svg-icons';
+import { faExclamationTriangle as warning } from '@fortawesome/free-solid-svg-icons';
+import { faExclamationCircle as error } from '@fortawesome/free-solid-svg-icons';
+import { faCheck as good } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle as complete } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner as progress } from '@fortawesome/free-solid-svg-icons';
 import { BarChart } from '../bar-chart/bar-chart.model';
 
 @Component({
@@ -10,10 +15,17 @@ import { BarChart } from '../bar-chart/bar-chart.model';
 export class DashboardViewComponent {
   pastWeek: number[];
   total: number;
-  public messages: string[];
+  public messages: Array<any>;
+  public completed: Array<any>;
+  // public inProgress: Array<any>;
   public photos: Array<BarChart>;
 
   camera = camera;
+  error = error;
+  warning = warning;
+  good = good;
+  complete = complete;
+  progress = progress;
 
   count = 0;
   d = new Date().getDay();
@@ -21,15 +33,108 @@ export class DashboardViewComponent {
   constructor() {
     this.pastWeek = [3, 5, 2, 3, 2, 1, 7];
     this.total = 253;
-    this.messages = ["Everything is good", "System back online", "System has halted!!!"];
+    this.messages = [
+      {
+        message: 'Everything is good',
+        icon: good,
+        color: 'green-icon',
+        date: "Today"
+      },
+      {
+        message: 'System is slower than usual',
+        icon: warning,
+        color: 'orange-icon',
+        date: "Yesterday"
+      },
+      {
+        message: 'Everything is good',
+        icon: good,
+        color: 'green-icon',
+        date: "10 May"
+      },
+      {
+        message: 'Fire Detected',
+        icon: error,
+        color: 'red-icon',
+        date: "9 May"
+      },
+      {
+        message: 'Unknown objects in image',
+        icon: warning,
+        color: 'orange-icon',
+        date: "8 May"
+      },
+    ];
+    this.completed = [
+      {
+        name: 'Everything is good',
+        date: "Today"
+      },
+      {
+        name: 'System is slower than usual',
+        date: "Yesterday"
+      },
+      {
+        name: 'Everything is good',
+        date: "10 May"
+      },
+      {
+        name: 'Fire Detected',
+        date: "9 May"
+      },
+      {
+        name: 'Unknown objects in image',
+        date: "8 May"
+      },
+      {
+        name: 'Unknown objects in image',
+        date: "8 May"
+      },
+      {
+        name: 'Unknown objects in image',
+        date: "8 May"
+      },
+      {
+        name: 'Unknown objects in image',
+        date: "8 May"
+      },
+    ];
     this.photos = [
-      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
-      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
-      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
-      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
-      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
-      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
-      { Value: this.pastWeek[this.count++], Size: '', Legend: this.getDayOfWeek() },
+      {
+        Value: this.pastWeek[this.count++],
+        Size: '',
+        Legend: this.getDayOfWeek(),
+      },
+      {
+        Value: this.pastWeek[this.count++],
+        Size: '',
+        Legend: this.getDayOfWeek(),
+      },
+      {
+        Value: this.pastWeek[this.count++],
+        Size: '',
+        Legend: this.getDayOfWeek(),
+      },
+      {
+        Value: this.pastWeek[this.count++],
+        Size: '',
+        Legend: this.getDayOfWeek(),
+      },
+      {
+        Value: this.pastWeek[this.count++],
+        Size: '',
+        Legend: this.getDayOfWeek(),
+      },
+      {
+        Value: this.pastWeek[this.count++],
+        Size: '',
+        Legend: this.getDayOfWeek(),
+      },
+      {
+        Value: this.pastWeek[this.count++],
+        Size: '',
+        Legend: this.getDayOfWeek(),
+      },
     ];
   }
 
