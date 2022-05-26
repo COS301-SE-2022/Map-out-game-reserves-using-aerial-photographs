9c014e461908246f5473e3fd94a267bd3ae01b1b
OomBen <u17030553@tuks.co.za>
2022-05-26 22:19:40 +0200

    Fixed http requests and server setup to comply with architecture

--- aerial-mapping/libs/api/shell/feature/src/lib/api-shell-feature.module.ts
+++ aerial-mapping/libs/api/shell/feature/src/lib/api-shell-feature.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { DashboardResolver } from '@aerial-mapping/api/dashboard/api/feature';
 import { PrismaService } from '@aerial-mapping/api/shared/services/prisma/data-access';
 import { DashboardRepository, ApiDashboardRepositoryModule } from '@aerial-mapping/api/dashboard/repository/data-access';
+import { S3UploadResolver } from '@aerial-mapping/api/s3-upload/api/feature';
+import { S3UploadRepository, S3UploadRepositoryModule } from '@aerial-mapping/api/s3-upload/repository/data-access';
 
 @Module({
   imports: [],
@@ -9,6 +11,9 @@ import { DashboardRepository, ApiDashboardRepositoryModule } from '@aerial-mappi
     DashboardRepository,
     ApiDashboardRepositoryModule,
     DashboardResolver,
+    S3UploadRepository,
+    S3UploadRepositoryModule,
+    S3UploadResolver,
     PrismaService],
   exports: []
 })
