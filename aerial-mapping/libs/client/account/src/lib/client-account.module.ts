5ab6163a6ec16c86a28a899e14f133bae920b6f4
Zoe Liebenberg <u20444738@tuks.co.za>
2022-05-28 14:04:22 +0200

    (ui): Accounts Page

--- aerial-mapping/libs/client/account/src/lib/client-account.module.ts
+++ aerial-mapping/libs/client/account/src/lib/client-account.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AccountComponent } from './account/account.component';
 import { ComponentsNavbarModule } from '@aerial-mapping/client/shared/components/navbar';
 import { AccountRoutingModule } from './account-routing.module';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   imports: [
     CommonModule,
     ComponentsNavbarModule,
-    AccountRoutingModule
+    AccountRoutingModule,
+    MatCardModule
   ],
   declarations: [AccountComponent],
   exports: [AccountComponent]
