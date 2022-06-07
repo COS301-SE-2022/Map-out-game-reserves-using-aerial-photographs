b8263bcc079413ac27087a67688a85fb4f7ec742
Dylan Spies <u20432748@tuks.co.za>
2022-06-07 17:24:09 +0200

    Revert "Create map-grid component"
    This reverts commit f726143033d638005bd49acff56b831329811c84.

--- aerial-mapping/libs/client/map-grid/src/lib/client-map-grid.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MapGridComponent } from './map-grid/map-grid.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [MapGridComponent],
-})
-export class ClientMapGridModule {}
