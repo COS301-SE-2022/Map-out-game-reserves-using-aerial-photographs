c9a4218ba562915ff897cf9a8db631dbff5931e0
Dylan Spies <u20432748@tuks.co.za>
2022-06-07 17:40:45 +0200

    Created map component

---
+++ aerial-mapping/libs/client/map/src/lib/client-map.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapViewComponent } from './map-view/map-view.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [MapViewComponent],
+})
+export class ClientMapModule {}
---
+++ aerial-mapping/libs/client/map/src/lib/map-view/map-view.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'aerial-mapping-map-view',
+  templateUrl: './map-view.component.html',
+  styleUrls: ['./map-view.component.scss'],
+})
+export class MapViewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
