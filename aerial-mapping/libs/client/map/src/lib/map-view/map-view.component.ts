ad518cad9c36b3dce6e633faa335009cd2dc1813
Dylan Spies <u20432748@tuks.co.za>
2022-06-08 13:06:55 +0200

    Map Exit Button and new Images

--- aerial-mapping/libs/client/map/src/lib/map-view/map-view.component.ts
+++ aerial-mapping/libs/client/map/src/lib/map-view/map-view.component.ts
@@ -1,11 +1,12 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { faXmark as exit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'aerial-mapping-map-view',
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.scss'],
 })
-export class MapViewComponent implements AfterViewInit  {
+export class MapViewComponent implements AfterViewInit {
   scale = 100;
   top = 0;
   left = 0;
@@ -14,6 +15,8 @@ export class MapViewComponent implements AfterViewInit  {
   startX = 0;
   startY = 0;
   d = false;
+  exit = exit;
+
   @ViewChild('map') map!: ElementRef;
 
   scroll = (ev: WheelEvent) => {
@@ -37,20 +40,18 @@ export class MapViewComponent implements AfterViewInit  {
   };
 
   drag = (ev: MouseEvent) => {
-
-    if (this.d == true){
-      this.left = this.originalX + (this.startX - ev.clientX);
-      this.top = this.originalY + (this.startY - ev.clientY);
+    if (this.d == true) {
+      this.left = Math.max(this.originalX + (this.startX - ev.clientX), 0);
+      this.top = Math.max(this.originalY + (this.startY - ev.clientY), 0);
     }
   };
 
-  
   ngAfterViewInit() {
     if (this.map) {
-    this.map.nativeElement.addEventListener('wheel', this.scroll);
-    this.map.nativeElement.addEventListener('mousedown', this.downListener);
-    this.map.nativeElement.addEventListener('mousemove', this.drag);
-    window.addEventListener('mouseup', this.upListener);
+      this.map.nativeElement.addEventListener('wheel', this.scroll);
+      this.map.nativeElement.addEventListener('mousedown', this.downListener);
+      this.map.nativeElement.addEventListener('mousemove', this.drag);
+      window.addEventListener('mouseup', this.upListener);
     }
   }
 }
