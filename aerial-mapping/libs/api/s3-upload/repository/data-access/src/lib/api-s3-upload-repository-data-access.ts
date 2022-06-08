7f177ca6ebaa211bf9438d2ccb127096f159f348
Steven Schormann <u20430516@tuks.co.za>
2022-06-08 13:10:02 +0200

    Revert "S3 connection"
    This reverts commit 6a905de49230d0224cc9762c2e291f6a0c340ae7.

--- aerial-mapping/libs/api/s3-upload/repository/data-access/src/lib/api-s3-upload-repository-data-access.ts
+++ aerial-mapping/libs/api/s3-upload/repository/data-access/src/lib/api-s3-upload-repository-data-access.ts
@@ -1,65 +1,81 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '@aerial-mapping/api/shared/services/prisma/data-access';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@aerial-mapping/api/shared/services/prisma/data-access";
 import dotenv = require('dotenv');
 import aws = require('aws-sdk');
-import { Prisma } from '@prisma/client';
+import { Prisma } from "@prisma/client";
+
 import { randomBytes } from 'crypto';
 
 dotenv.config();
 
-const region = 'us-east-1';
+const region = "eu-west-2";
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-const bucketName = 'dylpickles-s3-aerialmapping';
+const bucketName = "dylpickles-s3-aerialmapping";
 
 @Injectable()
 export class S3UploadRepository {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
-  public async createImage(
-    collectionId: number,
-    bucket_name: string,
-    file_name: string,
-  ) {
+  //Creates new S3-bucket object to into bucket
+  s3 = new aws.S3({
+    region,
+    accessKeyId,
+    secretAccessKey,
+    signatureVersion: 'v4'
+  })
+
+  generateUploadURL() {
+    const rawBytes = randomBytes(16);
+    const toHex = rawBytes.toString('hex');
+    const imageName = toHex + '.png';
+
+    const params = ({
+      Bucket: bucketName,
+      Key: imageName,
+      Expires: 60
+    })
+
+    const uploadURL = this.s3.getSignedUrlPromise('putObject', params);
+    return uploadURL;
+  }
+
+  public async createImage(collectionId: number, bucket_name: string, file_name: string) {
     await this.prisma.images.create({
       data: {
         collectionID: collectionId,
         bucket_name: bucket_name,
-        file_name: file_name,
-      },
-    });
-    return 'Success!';
+        file_name: file_name
+      }
+    })
+
+    return "Success!";
   }
 
-  public async getImage(imageID: number) {
+  public async getImage(bucket_name: string, file_name: string) {
     return this.prisma.images.findFirst({
       select: {
         imageID: true,
         collectionID: true,
         bucket_name: true,
-        file_name: true,
+        file_name: true
       },
       where: {
-        imageID: imageID,
-      },
+        bucket_name: bucket_name,
+        file_name: file_name
+      }
     });
   }
 
   public async getImagesByCollectionId(id: number) {
     return this.prisma.images.findMany({
       where: {
-        collectionID: id,
-      },
+        collectionID: id
+      }
     });
   }
 
-  public async createImageCollection(
-    parkID: number,
-    name: string,
-    dateTime: string,
-    completed: boolean,
-    flightID: number
-  ) {
+  public async createImageCollection(parkID: number, name: string, dateTime: string, completed: boolean, flightID: number) {
     //validation
     try {
       await this.prisma.image_Collection.create({
@@ -68,19 +84,21 @@ export class S3UploadRepository {
           name: name,
           upload_date_time: dateTime,
           completed: completed,
-          flightID: flightID,
-        },
+          flightID: flightID
+        }
       });
-      return 'Created Image Collection!';
-    } catch (e) {
-      if (e instanceof Prisma.PrismaClientKnownRequestError) {
-        if (e.code === 'P2002') {
-          return 'There is a unique constraint violation';
-        } else if (e.code === 'P2003') {
-          return 'There is a foreign key constraint violation';
+      return "Created Image Collection!";
+    }
+    catch(e) {
+      if(e instanceof Prisma.PrismaClientKnownRequestError) {
+        if(e.code === 'P2002') {
+          return "There is a unique constraint violation";
+        }
+        else if (e.code === 'P2003') {
+          return "There is a foreign key constraint violation";
         }
       }
-      return 'Prisma error';
+      return "Prisma error";
     }
   }
 }
