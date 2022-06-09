9db7a1bcc4b564986f6c13429b6c8e6bfd0c8251
Steven Schormann <u20430516@tuks.co.za>
2022-06-09 18:57:44 +0200

    Added upload feature

--- aerial-mapping/libs/client/shared/services/lib/client-api.service.ts
+++ aerial-mapping/libs/client/shared/services/lib/client-api.service.ts
@@ -1,4 +1,8 @@
-import { HttpClient, HttpContext, HttpContextToken, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpContextToken,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -6,22 +10,22 @@ export const IS_CACHE_ENABLED = new HttpContextToken<boolean>(() => false);
 
 @Injectable()
 export class ClientApiService {
-  token: string = "";
+  token: string = '';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // QUERIES //
 
-  runQuery(query:string,variables: any, token?: string): Observable<any> {
+  runQuery(query: string, variables: any, token?: string): Observable<any> {
     const options = {
       headers: new HttpHeaders({
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + token
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
       }),
     };
 
     return this.http.post<any>(
-      "http://localhost:3333/graphql",
+      'http://localhost:3333/graphql',
       JSON.stringify({
         query: query,
         variables: variables,
@@ -30,9 +34,12 @@ export class ClientApiService {
     );
   }
 
-
   getImageCollections(): Observable<any> {
-    return this.runQuery('query { getImageCollections { collectionID, parkID, upload_date_time, completed, flightID }}',null, this.token);
+    return this.runQuery(
+      'query { getImageCollections { collectionID, parkID, upload_date_time, completed, flightID }}',
+      null,
+      this.token
+    );
   }
 
   getImagesByCollectionId(id: number): Observable<any> {
@@ -40,7 +47,11 @@ export class ClientApiService {
   }
 
   getMessages(): Observable<any> {
-    return this.runQuery('query { getMessages { message_status, message_description,collectionID }}',null, this.token);
+    return this.runQuery(
+      'query { getMessages { message_status, message_description,collectionID }}',
+      null,
+      this.token
+    );
   }
 
   getAuthStatus(token: string): Observable<any> {
@@ -48,7 +59,10 @@ export class ClientApiService {
   }
 
   getImage(imageID: number): Observable<any> {
-    return this.runQuery('query ($imageID: Int){ getImage(imageID: $imageID) { imageID, collectionID, bucket_name, file_name }}', {imageID: imageID});
+    return this.runQuery(
+      'query ($imageID: Int){ getImage(imageID: $imageID) { imageID, collectionID, bucket_name, file_name }}',
+      { imageID: imageID }
+    );
   }
 
   getImageData(bucket_name: string, file_name: string): Observable<any> {
@@ -59,41 +73,120 @@ export class ClientApiService {
     };
 
     return this.http.get(
-      "https://3dxg59qzw5.execute-api.us-east-1.amazonaws.com/test_stage/"+bucket_name+"/"+file_name, {
+      'https://3dxg59qzw5.execute-api.us-east-1.amazonaws.com/test_stage/' +
+        bucket_name +
+        '/' +
+        file_name,
+      {
         headers: { 'Content-Type': 'image/png' },
-        responseType: 'text'
+        responseType: 'text',
       }
     );
   }
 
-
   // MUTATIONS //
+  uploadImage(
+    collectionID: number,
+    bucket_name: string,
+    file_name: string,
+    imgData: Observable<any>
+  ): Observable<any> {
+    this.createImage(collectionID, bucket_name, file_name).subscribe({
+      error: (err) => {
+        console.log(err);
+      },
+    });
 
-  createImageCollection(parkID: number): Observable<any> {
-    const now = new Date().toISOString();
+    return this.http.put(
+      'https://3dxg59qzw5.execute-api.us-east-1.amazonaws.com/test_stage/' +
+        bucket_name +
+        '/' +
+        file_name,
+      {
+        headers: {
+          'Content-Type': 'image/png',
+        },
+        observe: imgData,
+      }
+    );
+  }
 
-    return this.runQuery('mutation ($parkID: Int, $datetime: DateTime){ createImageCollection(parkID: $parkID, datetime: $datetime, flightID: $flightID) }', { parkID: parkID, datetime: now });
+  createBucket(
+    parkID: number, name: string, flightID: number, bucketID: number
+  ): Observable<any> {
+    this.createImageCollection(parkID,name,flightID).subscribe({
+      error: (err) => {
+        console.log(err);
+      },
+    });
+
+    return this.http.put(
+      'https://3dxg59qzw5.execute-api.us-east-1.amazonaws.com/test_stage/dylpickles-catalog-'+bucketID,
+      {
+        headers: {
+          'Content-Type': 'text/json',
+        }
+      }
+    );
+  }
+
+  createImage(
+    collectionID: number,
+    bucket_name: string,
+    file_name: string
+  ): Observable<any> {
+    const variables = {
+      collectionID: collectionID,
+      bucket_name: bucket_name,
+      file_name: file_name,
+    };
+
+    return this.runQuery(
+      'mutation ($collectionID: Int, $bucket_name: String, $file_name: String){ createImage(collectionID: $collectionID, bucket_name: $bucket_name, file_name: $file_name) }',
+      variables
+    );
+  }
+
+  createImageCollection(parkID: number, name: string, flightID: number): Observable<any> {
+    return this.runQuery(
+      'mutation ($parkID: Int, $name: String, $flightID: Int){ createImageCollection(parkID: $parkID, name: $name, flightID: $flightID) }',
+      { parkID: parkID, name: name, flightID: flightID }
+    );
   }
 
   login(email: string, password: string) {
-    return this.runQuery('mutation ($email: String, $password: String){ login(email: $email, password: $password) }', { email: email, password: password });
+    return this.runQuery(
+      'mutation ($email: String, $password: String){ login(email: $email, password: $password) }',
+      { email: email, password: password }
+    );
   }
 
   invite(email: string): Observable<any> {
-    return this.runQuery('mutation ($email: String){ invite(email: $email) }', { email: email });
+    return this.runQuery('mutation ($email: String){ invite(email: $email) }', {
+      email: email,
+    });
   }
 
-  registerUser(fname: string, lname: string, email: string, pass: string, role: string, approved: boolean): Observable<any> {
+  registerUser(
+    fname: string,
+    lname: string,
+    email: string,
+    pass: string,
+    role: string,
+    approved: boolean
+  ): Observable<any> {
     const variables = {
       firstname: fname,
       lastname: lname,
       email: email,
       password: pass,
       role: role,
-      approved: approved
-    }
+      approved: approved,
+    };
 
-    return this.runQuery('mutation ($firstname: String, $lastname: String, $email: String, $password: String, $role: String, $approved: Boolean){ registerUser(firstname: $firstname, lastname: $lastname, email: $email, password: $password, role: $role, approved: $approved) }', variables);
+    return this.runQuery(
+      'mutation ($firstname: String, $lastname: String, $email: String, $password: String, $role: String, $approved: Boolean){ registerUser(firstname: $firstname, lastname: $lastname, email: $email, password: $password, role: $role, approved: $approved) }',
+      variables
+    );
   }
-
 }
