558a019dab6f5c7c0a5af10ed2bd7c747a6244f3
Dylan Spies <u20432748@tuks.co.za>
2022-06-09 18:14:34 +0200

    Fixed Account Tests

--- aerial-mapping/libs/client/account/src/lib/account/account.component.spec.ts
+++ aerial-mapping/libs/client/account/src/lib/account/account.component.spec.ts
@@ -1,6 +1,10 @@
+import { ComponentsNavbarModule } from '@aerial-mapping/client/shared/components/navbar';
+import { ClientApiService } from '@aerial-mapping/client/shared/services';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { AccountComponent } from './account.component';
+
 
 describe('AccountComponent', () => {
   let component: AccountComponent;
@@ -8,6 +12,8 @@ describe('AccountComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ComponentsNavbarModule, RouterTestingModule, HttpClientModule],
+      providers: [ClientApiService, HttpClient],
       declarations: [AccountComponent],
     }).compileComponents();
   });
