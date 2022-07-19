2ae4fbe8f802d2e9ca6db8c042c8eb2e564749c0
Steven Schormann <u20430516@tuks.co.za>
2022-07-20 00:19:46 +0200

    Added s3 upload and download

--- src/app/api/controller/controller.service.ts
+++ src/app/api/controller/controller.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Auth } from 'aws-amplify';
+import { Auth, Storage } from 'aws-amplify';
 import { APIService, CreateFlightDetailsInput, CreateGameParkInput, CreateImageCollectionInput, CreateMessageInput, CreateUserInput, DeletePendingInvitesInput, User } from '../../API.service';
 import { v4 as uuidv4 } from 'uuid';
 const bcrypt = require('bcryptjs');
@@ -74,6 +74,22 @@ export class ControllerService {
           });
       });
     });
+  }
+
+  async S3upload(fileData: File){
+    const result = await Storage.put(fileData.name, fileData, {
+      contentType: fileData.type,
+    });
+    console.log(21, result);
+  };
+
+  async S3download(fileKey:string,fetch_data:boolean){
+    // Storage.list('public/') // for listing ALL files without prefix, pass '' instead
+    // .then(result => console.log(result))
+
+    const result = await Storage.get(fileKey, { download: fetch_data });
+    console.log(result);
+    return result;
   }
 
   //------------------------------------------------------------------------
