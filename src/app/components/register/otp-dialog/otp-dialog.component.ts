caa4d082375d1c27b71aff1933f422ae4673da9b
dylan2894 <61905387+dylan2894@users.noreply.github.com>
2022-07-20 13:18:47 +0200

    Adding OTP dialog

---
+++ src/app/components/register/otp-dialog/otp-dialog.component.ts
@@ -0,0 +1,30 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  otp: number
+}
+
+@Component({
+  selector: 'app-otp-dialog',
+  templateUrl: './otp-dialog.component.html',
+  styleUrls: ['./otp-dialog.component.scss']
+})
+export class OtpDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<OtpDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onSubmit() {
+    if(this.data.otp == null || this.data.otp == undefined) {
+      return;
+    }
+    this.dialogRef.close(this.data.otp);
+  }
+}
