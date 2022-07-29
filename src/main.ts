e3984fe196fe8232ad5b71130c83266a5f689e99
Steven Schormann <u20430516@tuks.co.za>
2022-07-29 15:25:25 +0200

    auth fix for CI/CD

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { Amplify } from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+import Amplify from '@aws-amplify/core'
+import { Auth } from '@aws-amplify/auth'
+import awsconfig from './aws-exports'
+
+Amplify.configure(awsconfig)
+Auth.configure(awsconfig)
 
 if (environment.production) {
   enableProdMode();
