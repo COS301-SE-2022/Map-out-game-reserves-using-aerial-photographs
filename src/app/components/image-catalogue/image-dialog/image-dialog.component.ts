f87ade6609bca8c93ffae7ede49e017c1e068b25
chichi091 <u20632429@tuks.co.za>
2022-09-17 15:52:46 +0200

    ✨(backend): Add backend for deleting maps

--- src/app/components/image-catalogue/image-dialog/image-dialog.component.ts
+++ src/app/components/image-catalogue/image-dialog/image-dialog.component.ts
@@ -1,8 +1,9 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-import { APIService } from 'src/app/API.service';
-
+import { promises } from 'dns';
+import { APIService, DeleteImageCollectionInput, GetImagesByCollectionIdQuery, DeleteImagesInput } from 'src/app/API.service';
+import { ControllerService } from 'src/app/api/controller/controller.service';
 export interface CatalogData {
   completed: boolean | undefined,
   error: boolean | undefined,
@@ -27,6 +28,7 @@ export class ImageDialogComponent {
 
   constructor( private router : Router, private api: APIService,
     public dialogRef: MatDialogRef<ImageDialogComponent>,
+    private apiController: ControllerService,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
   ) {
     this.selectCatalogue = data.selectedCatalogue;
@@ -45,10 +47,31 @@ export class ImageDialogComponent {
   }
 
   onDeleteClick(tryAgain:boolean): void {
-    //add delete code here
-    // this.api.DeleteImageCollection(this.selectCatalogue.collectionID);
+    // Delete from S3
+      // ----- Not yet working completely -----
+    // this.apiController.S3delete(this.selectCatalogue.collectionID);
+    // this.apiController.S3delete("00265eba-6e41-45db-ab10-ee3a5cc98c84/");
+
+    // Delete images from database
+    this.api
+      .GetImagesByCollectionId(this.selectCatalogue.collectionID)
+      .then((value: GetImagesByCollectionIdQuery[]) => {
+        for (const v of value) {
+          let deleteID: DeleteImagesInput = {imageID: v.imageID};
+          this.api.DeleteImages(deleteID);
+        }
+      });
+
+    //Delete imageCollection from database
+    var deleteInput: DeleteImageCollectionInput = {collectionID: ''};
+    deleteInput.collectionID = this.selectCatalogue.collectionID;
+    this.api.DeleteImageCollection(deleteInput);
+
     if (tryAgain) {
       this.router.navigateByUrl('/create-map');
+      setTimeout(() => {
+        window.location.reload();
+      }, 1);
     }
     this.dialogRef.close();
   }
