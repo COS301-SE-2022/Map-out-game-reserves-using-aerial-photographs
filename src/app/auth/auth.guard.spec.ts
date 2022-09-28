0b5dcc302ca292fcc52d72ca1a662e8d9cda0961
Dylan Spies <dylanspies1.ds@gmail.com>
2022-09-28 20:55:39 +0200

    Remove small unused import

--- src/app/auth/auth.guard.spec.ts
+++ src/app/auth/auth.guard.spec.ts
@@ -1,12 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   let guard: any;
-  const authMock: any = jasmine.createSpyObj('AWSAmplifyWrapper', ['getAuth']);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
