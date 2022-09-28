2652610c5a374260edb7e8959c19216a365a8fa1
Steven Schormann <u20430516@tuks.co.za>
2022-09-28 20:41:15 +0200

    reformatting mapbox.component.ts

--- src/app/components/dashboard/mapbox/mapbox.component.ts
+++ src/app/components/dashboard/mapbox/mapbox.component.ts
@@ -1,7 +1,4 @@
-// import { Component, Input, OnInit } from '@angular/core';
-// import { BarChart } from './mapbox.model';
-
-import { Component, OnInit, ChangeDetectorRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -10,82 +7,46 @@ import * as mapboxgl from 'mapbox-gl';
   styleUrls: ['./mapbox.component.scss'],
 })
 export class MapboxComponent implements OnInit, AfterViewInit {
-  // map: mapboxgl.Map = new mapboxgl.Map;
-  // style = 'mapbox://styles/mapbox/streets-v11';
   style = 'mapbox://styles/mapbox/satellite-streets-v11';
   lat = -25.8825;
   lng = 28.2639;
   mapContainer: HTMLElement | undefined;
-  // map = new mapboxgl.Map();
 
-  getMarkers() {
-    const geoJson = [{
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Point',
-        'coordinates': ['-25.8825', '28.2639']
-      },
-      'properties': {
-        'message': 'Reit Vlei Nature Reserve'
-      }
-    }, {
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Point',
-        'coordinates': ['77.350048', '12.953847' ]
-      },
-      'properties': {
-        'message': 'bangulare'
-      }
-    }];
-    return geoJson;
-  }
-  
-  
-  constructor() { }
-  ngOnInit() : void{
-    
-  }
+  constructor() {}
+  ngOnInit(): void {}
 
   ngAfterViewInit() {
-    this.mapContainer =  (<HTMLElement>document.getElementById('map'));
-    (mapboxgl as typeof mapboxgl).accessToken = 'pk.eyJ1IjoiY2dvbmNhbHZlczA5MSIsImEiOiJjbDdvdjB4NnUxMTgxM29uYjB0djEwYWw4In0.Yazsev1lNiyqs-qyUD6-Pg'
-      const map = new mapboxgl.Map({
-        container: this.mapContainer,
-        style: this.style,
-        zoom: 11,
-        center: [this.lng, this.lat]
+    this.mapContainer = <HTMLElement>document.getElementById('map');
+    (mapboxgl as typeof mapboxgl).accessToken =
+      'pk.eyJ1IjoiY2dvbmNhbHZlczA5MSIsImEiOiJjbDdvdjB4NnUxMTgxM29uYjB0djEwYWw4In0.Yazsev1lNiyqs-qyUD6-Pg';
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: this.style,
+      zoom: 11,
+      center: [this.lng, this.lat],
     });
 
     map.addControl(new mapboxgl.NavigationControl());
 
-    const markers = this.getMarkers();
-    const data = {
-        type: 'FeatureCollection',
-        features: markers
-      };
-
-
-    map.on('load', ()=> {
+    map.on('load', () => {
       map.addSource('marker', {
         type: 'geojson',
         data: {
           type: 'FeatureCollection',
-          features: [{
-            'type': 'Feature',
-            'geometry': {
-              'type': 'Point',
-              'coordinates': [25.88089319102171, -28.263810842208677]
+          features: [
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [25.88089319102171, -28.263810842208677],
+              },
+              properties: {
+                message: 'Reit Vlei Nature Reserve',
+              },
             },
-            'properties': {
-              'message': 'Reit Vlei Nature Reserve'
-            }
-          }]
-        }
+          ],
+        },
       });
-      // const temp = map.getSource('marker');
-      // temp.setData(data);
-      // map.getSource('marker').setData(data);
       map.addLayer({
         id: 'customMarketid',
         source: 'marker',
@@ -95,13 +56,13 @@ export class MapboxComponent implements OnInit, AfterViewInit {
           'text-size': 24,
           'text-transform': 'uppercase',
           'icon-image': 'marker-15',
-          'text-offset': [0, 1.5]
+          'text-offset': [0, 1.5],
         },
         paint: {
           'text-color': '#f16624',
           'text-halo-color': '#fff',
-          'text-halo-width': 2
-        }
+          'text-halo-width': 2,
+        },
       });
     });
   }
