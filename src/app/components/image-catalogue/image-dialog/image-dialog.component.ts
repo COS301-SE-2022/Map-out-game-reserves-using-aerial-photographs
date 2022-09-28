ffc8fa9152ffc2aedb5563c14f0c77d31b3db187
Steven Schormann <u20430516@tuks.co.za>
2022-09-28 21:06:30 +0200

    reformatting image-dialog.component.ts

--- src/app/components/image-catalogue/image-dialog/image-dialog.component.ts
+++ src/app/components/image-catalogue/image-dialog/image-dialog.component.ts
@@ -1,11 +1,9 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-import { promises } from 'dns';
 import {
   APIService,
   DeleteImageCollectionInput,
-  GetImagesByCollectionIdQuery,
   DeleteImagesInput,
   GetMessageByCollectionIdQuery,
   DeleteMessageInput,
@@ -16,7 +14,6 @@ import {
   ConfirmDialogModel,
 } from './confirm-dialog/confirm-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
-import { ImageCatalogueComponent } from '../image-catalogue.component';
 import { Auth } from 'aws-amplify';
 
 export interface CatalogData {
@@ -46,11 +43,9 @@ export class ImageDialogComponent {
   constructor(
     private router: Router,
     private api: APIService,
-    private controller: ControllerService,
     public dialogRef: MatDialogRef<ImageDialogComponent>,
     private apiController: ControllerService,
     public dialog: MatDialog,
-    // private cataloguesRef: ImageCatalogueComponent,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
   ) {
     this.selectCatalogue = data.selectedCatalogue;
@@ -61,7 +56,6 @@ export class ImageDialogComponent {
           'spinner'
         ) as HTMLCollectionOf<HTMLElement>
       );
-      // console.log(this.spinners);
       this.spinners.forEach((spin) => {
         spin.style.display = 'none';
       });
@@ -82,10 +76,12 @@ export class ImageDialogComponent {
       });
   }
 
+  //closes the image-dialog
   onNoClick(): void {
     this.dialogRef.close();
   }
 
+  //deletes the selected image collection
   async onDeleteClick(tryAgain: boolean): Promise<void> {
     this.apiController.S3delete(
       this.selectCatalogue.collectionID + '/thumbnails/thumbnail_0'
@@ -96,12 +92,6 @@ export class ImageDialogComponent {
     this.apiController.S3delete(
       this.selectCatalogue.collectionID + '/thumbnails/thumbnail_2'
     );
-    // if(this.result==true) {
-    // Delete from S3
-    // ----- Not yet working completely -----
-    // TODO:
-    // this.apiController.S3delete(this.selectCatalogue.collectionID);
-    // this.apiController.S3delete("00265eba-6e41-45db-ab10-ee3a5cc98c84/");
 
     // Delete images from database
     this.selectCatalogue.images.forEach((imageData: any) => {
@@ -128,8 +118,6 @@ export class ImageDialogComponent {
       this.api
         .GetMessageByCollectionId(ID)
         .then((value: GetMessageByCollectionIdQuery) => {
-          // const temp:Array<> = value.items;
-          // console.log(value.items[0]?.messageID);
           let t: string | undefined = value.items[0]?.messageID;
           if (t == undefined) {
             t = '';
@@ -137,20 +125,13 @@ export class ImageDialogComponent {
           let deleteId: DeleteMessageInput = { messageID: t };
           this.api.DeleteMessage(deleteId);
         });
-      // this.snackbar.open("Error message dismissed", '❌', {
-      //   verticalPosition: 'bottom',
-      // });
       if (document.getElementById(ID) != null) {
         document.getElementById(ID)!.style.display = 'none';
       }
     }
-
-    // }
-    // else {
-
-    // }
   }
 
+  //opens the selected map
   onSubmit(taskID?: string) {
     if (taskID != null) {
       this.router.navigateByUrl('/map', { state: { taskID: taskID } });
@@ -161,12 +142,10 @@ export class ImageDialogComponent {
     this.dialogRef.close();
   }
 
+  //confirms if the user wants to delete the selected image collection
   confirmDialog(tryAgain: boolean): void {
-    // this.dialogRef.close();
     const message = `Are you sure you want to do delete this map?`;
-
     const dialogData = new ConfirmDialogModel('Confirm Action', message);
-
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
       maxWidth: '400px',
       data: dialogData,
@@ -174,7 +153,6 @@ export class ImageDialogComponent {
 
     dialogRef.afterClosed().subscribe((dialogResult) => {
       this.result = dialogResult;
-      // console.log(dialogResult);
       if (dialogResult === true) {
         this.onDeleteClick(tryAgain).then(() => {
           if (tryAgain) {
