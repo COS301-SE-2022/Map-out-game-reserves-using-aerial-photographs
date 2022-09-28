cdd8da000bea135f97b5f853e6ce46451b7f8381
Dylan Spies <dylanspies1.ds@gmail.com>
2022-09-28 20:23:35 +0200

    Fixing up map spec.ts files

--- src/app/components/map/safe.pipe.spec.ts
+++ src/app/components/map/safe.pipe.spec.ts
@@ -1,13 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { SafePipe } from './safe.pipe';
-import { SafeResourceUrl } from '@angular/platform-browser';
 
 describe('SafePipe', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ SafePipe ],
-      imports: [ BrowserModule ]
+      declarations: [SafePipe],
+      imports: [BrowserModule],
     });
   });
 
@@ -21,5 +20,5 @@ describe('SafePipe', () => {
     const domSanitizer = TestBed.inject(DomSanitizer);
     const pipe = new SafePipe(domSanitizer);
     expect(typeof pipe.transform('http://192.168.1.2:80/')).toBe('object');
-  })
+  });
 });
