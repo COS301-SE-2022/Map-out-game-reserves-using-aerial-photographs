554e944aa75588e1639a6a6d2c3880cee3da8ac2
Steven Schormann <u20430516@tuks.co.za>
2022-09-28 20:37:41 +0200

    reformatting account.component.ts

--- src/app/api/controller/controller.service.ts
+++ src/app/api/controller/controller.service.ts
@@ -19,7 +19,6 @@ export class ControllerService {
       'wss://ha3u3iiggc.execute-api.sa-east-1.amazonaws.com/production/'
     );
     this.websocket.onmessage = (msg: any) => {
-      console.log('SNS Message Received');
       msg = JSON.parse(msg);
       if (msg.status == 'error') {
         this.snackBar.open(
@@ -35,11 +34,9 @@ export class ControllerService {
       }
     };
     this.websocket.onclose = () => {
-      console.log('Websocket connection closed');
       setTimeout(this.configWebSocket, 250); //reopen websocket in 250ms
     };
     this.websocket.onopen = () => {
-      console.log('Websocket connection opened');
       this.websocket!.send(
         JSON.stringify({
           message: 'subscribe', //this selects the 'subscribe' API Gateway route (which triggers the onSubscribe lambda function)
@@ -56,7 +53,6 @@ export class ControllerService {
       'wss://ha3u3iiggc.execute-api.sa-east-1.amazonaws.com/production/'
     );
     this.websocket.onmessage = (msg: any) => {
-      console.log('SNS Message Received');
       msg = JSON.parse(msg);
       if (msg.status != null && msg.status == 'error') {
         this.snackBar.open(
@@ -72,11 +68,9 @@ export class ControllerService {
       }
     };
     this.websocket.onclose = () => {
-      console.log('Websocket connection closed - in configWebSocket()');
       setTimeout(this.configWebSocket, 250); //reopen websocket in 250ms
     };
     this.websocket.onopen = () => {
-      console.log('Websocket connection opened');
       this.websocket!.send(
         JSON.stringify({
           message: 'subscribe', //this selects the 'subscribe' API Gateway route (which triggers the onSubscribe lambda function)
@@ -103,7 +97,6 @@ export class ControllerService {
 
   //calls 'protected-signup' lambda function and tries to register user (if an invite exists)
   tryRegister(u: CreateUserInput): Observable<any> {
-    console.log('[CLIENT] Trying to register user...');
     const body = {
       username: u.user_email,
       password: u.user_password,
@@ -125,7 +118,6 @@ export class ControllerService {
       await Auth.signIn(u.user_email!, u.user_password!);
       return 1; //for testing purposes
     } catch (error) {
-      console.log('[CLIENT] error signing in:', error);
       return -1; //for testing purposes
     }
   }
@@ -139,14 +131,13 @@ export class ControllerService {
     dataType: string
   ) {
     try {
-      const result = await Storage.put(
+      await Storage.put(
         collection + '/' + folder + '/' + fileKey,
         fileData,
         {
           contentType: dataType,
         }
       );
-      console.log(21, result);
     } catch (e) {
       console.log('S3upload error: ', e);
     }
@@ -155,8 +146,7 @@ export class ControllerService {
   //deletes files from our s3 bucket
   async S3delete(collection: string) {
     try {
-      const result = await Storage.remove(collection);
-      console.log(21, result);
+      await Storage.remove(collection);
     } catch (e) {
       console.log('S3delete error: ', e);
     }
