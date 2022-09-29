9442c8c4357683d44b65d7cdaa837e1e3da81855
OomBen <u17030553@tuks.co.za>
2022-09-29 10:37:26 +0200

    Added Admin Page, removed rest

--- WebODM/app/api/admin.py
+++ WebODM/app/api/admin.py
@@ -5,10 +5,12 @@ from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
 from app import models
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model  = User
-        fields = '__all__' 
+        model = User
+        fields = "__all__"
+
 
 class AdminUserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
@@ -16,23 +18,26 @@ class AdminUserViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = User.objects.all()
-        email = self.request.query_params.get('email', None)
+        email = self.request.query_params.get("email", None)
         if email is not None:
             queryset = queryset.filter(email=email)
         return queryset
+
     def create(self, request):
         data = request.data.copy()
-        password = data.get('password')
-        data['password'] = make_password(password)
+        password = data.get("password")
+        data["password"] = make_password(password)
         user = UserSerializer(data=data)
         user.is_valid(raise_exception=True)
         user.save()
         return Response(user.data, status=status.HTTP_201_CREATED)
 
+
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
-        model  = Group
-        fields = '__all__'
+        model = Group
+        fields = "__all__"
+
 
 class AdminGroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
@@ -40,7 +45,7 @@ class AdminGroupViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = Group.objects.all()
-        name = self.request.query_params.get('name', None)
+        name = self.request.query_params.get("name", None)
         if name is not None:
             queryset = queryset.filter(name=name)
         return queryset
