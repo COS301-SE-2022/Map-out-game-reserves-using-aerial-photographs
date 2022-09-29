7f62c238cdf4c57e4cc8f8aa57dcc451faa4224f
OomBen <u17030553@tuks.co.za>
2022-09-29 10:37:58 +0200

    Added Auth

--- WebODM/app/api/authentication.py
+++ WebODM/app/api/authentication.py
@@ -3,4 +3,4 @@ from rest_framework_jwt.authentication import BaseJSONWebTokenAuthentication
 
 class JSONWebTokenAuthenticationQS(BaseJSONWebTokenAuthentication):
     def get_jwt_value(self, request):
-         return request.query_params.get('jwt')
+        return request.query_params.get("jwt")
